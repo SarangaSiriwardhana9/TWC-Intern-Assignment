3071314a23747f9ae84209aead3a18ce48729ac9
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-04-13 19:08:21 +0530

    Home page Curve corner updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-import SignOut from "../components/SignOut";
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex h-screen  overflow-hidden">
-        <div className="flex-1 bg-[#083F46] rounded-r-[50%]   pr-10 -my-20  overflow-hidden">
-          {/* Left side - Sign Up component */}
-          <div className="flex items-center justify-center h-full">
-            {/* Your SignUp component goes here */}
-          </div>
-        </div>
-        <div className="flex-1 bg-white rounded-l-[10%]">
-          {/* Right side - Simple text */}
-          <div className="flex items-center justify-center h-full">
-            <p className="text-2xl">Your simple text here</p>
-          </div>
-        </div>
+    <div className="bg-green-500 h-screen overflow-hidden relative">
+      <div className="absolute top-0 right-0 w-60 bg-white overflow-hidden">
+        <div className="h-60 bg-green-500" style={{ borderTopLeftRadius: '200px', transform: 'rotate(90deg)' }}></div>
+      </div>
+      <div className="absolute bottom-0 left-0 w-60 h-60 bg-white overflow-hidden">
+        <div className="h-60 bg-green-500" style={{ borderBottomRightRadius: '200px', transform: 'rotate(90deg)' }}></div>
       </div>
     </div>
   );
